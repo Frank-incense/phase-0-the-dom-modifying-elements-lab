62f6e729c0759af77bc590b531476ba5dbf80cfb
Frankincense <frankincensewelsey@gmail.com>
2025-02-27 09:22:03 +0300

    Dom manipulating exercise

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+// Test 1
+const main = document.getElementById("main");
+main.remove()
+
+// Test 2 
+const newHeader = document.createElement('h1');
+
+// Test 3
+newHeader.id = "victory";
+
+// test 4
+newHeader.textContent = "is the champion";
